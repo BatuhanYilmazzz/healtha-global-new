5ad1ecba11e7625cbac8893301784756d77746c1
batuhannyilmazzz <ahmet.yilmaz@getir.com>
2021-10-17 17:43:17 +0300

    updated email config

--- src/pages/home-page/Consultancy.js
+++ src/pages/home-page/Consultancy.js
@@ -1,49 +1,60 @@
-import React, { useState } from 'react';
-import { ConsultancyStyled } from '../../styles/pages';
-import { useTranslation } from 'react-i18next';
-import emailjs from 'emailjs-com';
+import React, { useState } from "react";
+import { ConsultancyStyled } from "../../styles/pages";
+import { useTranslation } from "react-i18next";
+import emailjs from "emailjs-com";
+import { emailjsInfo } from "../../constant";
+
 function Consultancy() {
   const { t } = useTranslation();
   const [state, setState] = useState({
-    name: '',
-    email: '',
-    service: '',
-    message: '',
+    name: "",
+    email: "",
+    service: "",
+    message: "",
   });
   const onChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
   var template_params = {
-    reply_to: 'reply_to_value',
+    reply_to: "reply_to_value",
     from_name: `${state.name}`,
-    to_name: 'Aksan Health',
+    to_name: "Aksan Health",
     message_html: `I am ${state.name} <br/> My email adress: ${state.email}<br/> I want to get service about ${state.service} <br/> Message: ${state.message}`,
   };
-  var service_id = 'aksanhealth';
-  var template_id = 'mainpage';
-  var user_id = 'user_w83tNLLtCjsDK9FeoB3ex';
+
   function sendEmail(e) {
     e.preventDefault();
-    emailjs.send(service_id, template_id, template_params, user_id);
-    alert('We will keep in touch you as soon as possible');
-    setState({
-      name: '',
-      email: '',
-      service: '',
-      message: '',
-    });
+    emailjs
+      .send(
+        emailjsInfo.service_id,
+        emailjsInfo.template_id,
+        template_params,
+        emailjsInfo.user_id
+      )
+      .then(() => {
+        alert("We will keep in touch you as soon as possible");
+        setState({
+          name: "",
+          email: "",
+          service: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        alert(error.text);
+      });
   }
   return (
     <ConsultancyStyled>
       <div className='container'>
-        <h1>{t('CONSULTANTY')}</h1>
+        <h1>{t("CONSULTANTY")}</h1>
         <div className='row'>
           <div className='col'>
             <form onSubmit={sendEmail}>
               <input
                 type='text'
-                placeholder={t('NAME_SURNAME')}
+                placeholder={t("NAME_SURNAME")}
                 name='name'
                 value={state.name}
                 onChange={onChange}
@@ -51,7 +62,7 @@ function Consultancy() {
               />
               <input
                 type='email'
-                placeholder={t('E_MAIL')}
+                placeholder={t("E_MAIL")}
                 value={state.email}
                 name='email'
                 onChange={onChange}
@@ -59,7 +70,7 @@ function Consultancy() {
               />
               <input
                 type='text'
-                placeholder={t('CHOOSE_TOPIC')}
+                placeholder={t("CHOOSE_TOPIC")}
                 value={state.service}
                 name='service'
                 onChange={onChange}
@@ -70,12 +81,12 @@ function Consultancy() {
                 id=''
                 cols='30'
                 rows='8'
-                placeholder={t('MESSAGE')}
+                placeholder={t("MESSAGE")}
                 onChange={onChange}
                 value={state.message}
                 required
               ></textarea>
-              <button type='submit'>{t('SEND')}</button>
+              <button type='submit'>{t("SEND")}</button>
             </form>
           </div>
         </div>
