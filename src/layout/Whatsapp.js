f447de9f78ab638204fa7ceee5f2f5226d8deecf
Ahmet Batuhan Yılmaz <54447779+BatuhanYilmazzz@users.noreply.github.com>
2022-06-01 23:44:13 +0300

    Update Whatsapp.js

--- src/layout/Whatsapp.js
+++ src/layout/Whatsapp.js
@@ -3,7 +3,7 @@ import { WhatsappStyled } from '../styles/components';
 
 function Whatsapp() {
   return (
-    <WhatsappStyled target='_blank' href='https://wa.me/05323997632'>
+    <WhatsappStyled target='_blank' href='https://wa.me/+905323997632'>
       <img src='/images/whatsapp.png' alt='whatsapp' />
     </WhatsappStyled>
   );
